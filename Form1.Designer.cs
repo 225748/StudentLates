26c64b0c2b4c26a2a4c73848de0d92c986171f06
Ben Franklin <225748@live.brock.ac.uk>
2024-08-18 20:36:52 +0100

    Up to adding DisplayData(); in form load. Ran program and it crashed due to missing Microsoft.ACE.OLEDB.12.0, see one note for more detail.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lstVStudents = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // lstVStudents
+            // 
+            this.lstVStudents.HideSelection = false;
+            this.lstVStudents.Location = new System.Drawing.Point(50, 76);
+            this.lstVStudents.Name = "lstVStudents";
+            this.lstVStudents.Size = new System.Drawing.Size(288, 201);
+            this.lstVStudents.TabIndex = 0;
+            this.lstVStudents.UseCompatibleStateImageBehavior = false;
+            this.lstVStudents.View = System.Windows.Forms.View.Details;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstVStudents);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView lstVStudents;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace StudentLates
 {
@@ -16,5 +17,29 @@ namespace StudentLates
         {
             InitializeComponent();
         }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DisplayData();
+        }
+
+        public void DisplayData()
+        {
+            clsDBConnector dbConnector = new clsDBConnector();
+            OleDbDataReader dr;
+            string sqlStr;
+            dbConnector.Connect();
+            sqlStr = "SELECT studentID, firstName, surName, studentDOB FROM tblStudent";
+            dr = dbConnector.DoSQL(sqlStr);
+            lstVStudents.Items.Clear();
+            while (dr.Read())
+            {
+                lstVStudents.Items.Add(dr[0].ToString());
+                lstVStudents.Items[lstVStudents.Items.Count - 1].SubItems.Add(dr[1].ToString());
+                lstVStudents.Items[lstVStudents.Items.Count - 1].SubItems.Add(dr[2].ToString());
+                lstVStudents.Items[lstVStudents.Items.Count - 1].SubItems.Add(dr[3].ToString());
+            }
+            dbConnector.Close();
+        }
+
     }
 }
